8b0bf0d0c6b6307eb53ac8b1152658ac5cfba50e
nuttamon <nuttamon21574@gmail.com>
2021-11-26 00:56:05 +0700

    a

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Nuttamon</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+	<link rel="icon" href="https://meditation-lab.com/wp-content/uploads/2019/06/icon.png">
+	<link href="https://fonts.googleapis.com/css?family=Maitree|Mali|Sarabun&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+	
+    <link rel="stylesheet" href="css.css">
+  
+
+</head>
+<body>
+<!--/////////////////////////////////////////////////////////////////////////////////-->
+  <!--เริ่ม Menu-->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Home</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Activity</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Ability</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contract</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+<!--/////////////////////////////////////////////////////////////////////////////////-->
+
+
+<!--/////////////////////////////////////////////////////////////////////////////////-->
+  <section id="profile">
+    <br>
+    <h1 style="font-size: 45px; color: #0083a1; font-family: 'Mali', cursive; text-align: center;">Portfolio</h1>
+    <hr>
+<!--/////////////////////////////////////////////////////////////////////////////////-->
+      <!--เริ่มการแบ่งแถว (1 แถวจะมี 3 Column)-->
+      <div class="row">
+
+        <!-- เริ่มการแบ่ง Column ที่1-->
+        <div class="col-md-4">
+          <div id="descImg"><h3 class="font-maitree mb-3 mt-3"; style="text-align: center;">กิจกรรมวันแม่</h3></div>
+          <img src="https://i.ibb.co/9NMJWvm/1615892486398.jpg" id="topic-cover" class="img-thumbnail" alt="pict">
+          <p class="mt-1">In the Simple Tasks sample application, the HTML-service page Page.html loads data this way, and only uses templating to include other source code into the page.</p>
+        </div>
+
+        <!-- เริ่มการแบ่ง Column ที่2-->
+        <div class="col-md-4">
+          <div id="descImg"><h3 class="font-maitree mb-3 mt-3"; style="text-align: center;">กิจกรรมวันแม่</h3></div>
+          <img src="https://i.ibb.co/8Yj9p2d/1611588575485.jpg" id="topic-cover" class="img-thumbnail" alt="pict">
+          <p class="mt-1">In the Simple Tasks sample application, the HTML-service page Page.html loads data this way, and only uses templating to include other source code into the page.</p>
+        </div>
+
+        <!-- เริ่มการแบ่ง Column ที่3-->
+        <div class="col-md-4">
+          <div id="descImg"><h3 class="font-maitree mb-3 mt-3"; style="text-align: center;">กิจกรรมวันแม่</h3></div>
+          <img src="https://i.ibb.co/9NMJWvm/1615892486398.jpg" id="topic-cover" class="img-thumbnail" alt="pict">
+          <p class="mt-1">In the Simple Tasks sample application, the HTML-service page Page.html loads data this way, and only uses templating to include other source code into the page.</p>
+        </div>              
+
+      </div>
+      <!-- สิ้นสุดการแบ่งแถว (1 แถวจะมี 3 Column)-->
+<!--/////////////////////////////////////////////////////////////////////////////////-->
+
+
+
+  </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--/////////////////////////////////////////////////////////////////////////////////-->
+  <!--ส่วนของ Section-->
+	<section id="meditation">
+
+		<h3>เหตุผลที่อยากเข้าคณะนี้</h3>
+		<h3>มีอะไรบ้างมาดูกัน..<i class='bx bx-happy-beaming' ></i></h3>
+
+		<div id="benefits">
+			<h3 class="font-maitree">1.ช่วยให้จิตใจผ่องใส อ่อนโยน</h3>
+			<p>การทำสมาธิจะช่วยให้จิตใจเรานิ่งสงบ ท่ามกลางจิตที่กำหนดภายใต้ลมหายใจเข้าออก เราจะรู้ตัวตลอดเวลา สามารถตามทันทุกอารมณ์ ยิ่งเรากำหนดลมหายใจเข้าออกอย่างช้าๆ ลึกๆ ด้วยแล้ว มันจะยิ่งช่วยกล่อมเกลาจิตใจเราในตัว ทำให้เราเป็นคนสุขุมนุ่มลึก ใจเย็น อ่อนโยนและมองโลกรอบตัวในแง่ดีงามมากขึ้น อารมณ์ก็จะแจ่มใส และยังส่งผลให้สุขภาพดีตามมาอีกด้วย</p>
+
+			<h3>2.ช่วยผ่อนคลายความเครียด</h3>
+			<p>การทำสมาธิเมื่อจิตใจเรานิ่งมากๆ ผสานเป็นหนึ่งเดียวกับลมหายใจแล้ว สารแห่งความสุขจะหลั่งไหลออกมาจนแผ่ซ่านความเย็นไปทั่วสรรพางค์กายอย่างน่าอัศจรรย์ใจทีเดียวค่ะ ด้วยเหตุนี้ จึงช่วยผ่อนคลายความเครียดได้อย่างปลิดทิ้งทันตาและยังทำให้เรามีจิตใจสงบเยือกเย็น และมีความสุขอย่างที่ไม่เคยสัมผัสมาก่อนแน่นอน</p>
+
+			<h3>3.ลดอารมณ์โกรธหรือโมโหร้ายได้</h3>
+			<p>การหมั่นฝึกสมาธิบ่อยๆ จะทำให้เรารู้เท่าทันอารมณ์ของตนเองได้ดี เพราะจิตเราจะอยู่กับลมหายใจตลอดเวลา เราจะรู้ทุกการเคลื่อนไหว ทุกสิ่งที่คิด พูดและทำจะมาพร้อมสติอยู่เสมอ เมื่อไรที่เราเริ่มโกรธใครหรือกำลังโมโหร้ายหงุดหงิด เชื่อไหมว่าจิตเบื้องลึกที่ผ่านการทำสมาธิมาแล้วอย่างดีจะสอนเตือนตัวมันเอง ทำให้เราดึงอารมณ์ให้กลับมาอยู่กับปัจจุบันขณะ อยู่กับลมหายใจ และหากเราทำได้เช่นนี้บ่อยๆ ก็ย่อมเปลี่ยนจากคนที่มักมีอารมณ์ร้าย เกรี้ยวกราดง่ายมาเป็นคนที่ใจเย็นลงได้มากขึ้นแนนอน</p>
+
+			<h3>4.ช่วยให้ความจำดีและสมองทำงานมีประสิทธิภาพมากขึ้น</h3>
+			<p>สำหรับนักเรียน นักศึกษาหรือแม้แต่คนวัยทำงานที่จะต้องใช้สมองในการคิด วางแผนและใช้ไอเดียมากๆ นั้น การหมั่นทำสมาธิบ่อยๆ ย่อมช่วยเสริมสร้างทั้งความจำดี ช่วยกระตุ้นให้เกิดสมาธิ ทำให้สมองทำงานอย่างมีประสิทธิภาพมากขึ้นได้ เพราะจิตที่ผ่านการฝึกมาจนเกิดสมาธิจะทำให้ความคิดของเราที่คิดและทำ กลั่นออกมาจากลมหายใจอันประณีต สติจะสอนสั่งให้เราดำเนินชีวิตอย่างละเมียดละไมมากขึ้น ดังนั้นแล้ว หากใครอยากให้คุณภาพชีวิตดีขึ้นทุกด้าน ให้การทำงานของศักยภาพสมองเป็นไปอย่างมีคุณภาพ ฝึกสมาธิบ่อยๆ สิคะ ..ช่วยได้แน่นอน</p>
+
+			<h3>5.ช่วยให้ผิวพรรณอ่อนเยาว์ ผ่องใส ดูเด็กกว่าวัย</h3>
+			<p>เวลาที่เราฝึกสมาธิ เราจะต้องกำหนดลมหายใจเข้าออก โดยสูดหายใจเข้าไปให้เต็มท้องลึกๆ ช้าๆ และปล่อยลมหายใจออกมายาวๆ เมื่อทำแบบนี้สม่ำเสมอ ก็ย่อมช่วยให้ออกซิเจนเข้ามาฟอกเลือดภายในร่างกายได้ เป็นการดีท็อกซ์สารพิษในตัว ส่งผลให้สมองไบรท์ คิดอะไรก็ลื่นไหลและสารแห่งความสุขที่หลั่งออกมาก็ทำให้เรามีความสุข บำบัดทุกข์ ผ่อนคลายความเครียดได้ผล แน่นอนค่ะว่านี่คือ ยาอายุวัฒนะของจริงแถมยังส่งผลให้ผิวพรรณสะอาด ผ่องใส เลือดลมไหลเวียนดี ผิวเต่งตึงจนอ่อนเยาว์กว่าวัยในที่สุด</p>
+		</div>
+	</section>
+  
+	<section id="summary">
+		<h2 class="font-dark-red font-maitree">สรุป</h2>
+		<p>เป็นอย่างไรกันบ้างคะ สำหรับโยชน์จากการทำสมาธิ ใครที่ไม่รู้ว่าทำไมคนเราจะต้องทำสมาธิ ทำแล้วมีผลดียังไง? ช่วยอะไรได้บ้าง หวังว่าคำตอบจากเราดังกล่าวจะเป็นคำตอบที่ดีให้คุณได้และทำให้คุณมีคุณภาพชีวิตที่ดีขึ้นและมีความสุขบนโลกนี้อย่างราบรื่นต่อไป</p>
+	</section>
+
+</body>
+</html>
